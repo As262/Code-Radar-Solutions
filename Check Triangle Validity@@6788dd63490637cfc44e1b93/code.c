62456981eda6997123a83bd30223d8ab6ce01080
Avinav Saini <141184593+As262@users.noreply.github.com>
2025-01-23 05:46:54 +0530

    Solution to Check Triangle Validity

--- Check Triangle Validity@@6788dd63490637cfc44e1b93/code.c
+++ Check Triangle Validity@@6788dd63490637cfc44e1b93/code.c
@@ -3,10 +3,7 @@
 int main(){
     int a,b,c;
     scanf("%i%i%i",&a,&b,&c);
-    int x=(int)pow(a,2);
-    int y = (int)pow(b,2);
-    int z =(int)pow(c,2);
-    if(x+y==z &&y+z==x&&x+z==y){
+    if(a+b>c &&a+c>b&&b+c>a){
         printf("Valid");
     }
     else{
