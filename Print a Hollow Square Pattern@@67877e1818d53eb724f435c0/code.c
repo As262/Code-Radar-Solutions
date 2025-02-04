a3c22d92e1b51fec416eb5847d82ce7922a467ad
Avinav Saini <141184593+As262@users.noreply.github.com>
2025-02-04 22:10:23 +0530

    Update code.c

--- Print a Hollow Square Pattern@@67877e1818d53eb724f435c0/code.c
+++ Print a Hollow Square Pattern@@67877e1818d53eb724f435c0/code.c
@@ -2,15 +2,12 @@
 int main(){
     int a;
     scanf("%i",&a);
-    for(int i=1;i<=a;i++){
-        for (int j = 0;j<a-i;j++){
-            printf("*");
-        }
-        for(int j = 1;j<=2*i-1;j++){
-            printf(" *");
-        }
-        printf("\n");
-    }
+        for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i == 0 || i == n - 1 || j == 0 || j == n - 1) {
+                printf("*");
+            } else {
+                printf(" ");
     return 0;
     
 }
