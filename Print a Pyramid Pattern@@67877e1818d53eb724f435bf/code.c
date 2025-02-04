89c7a59d0c82f040c169494ee46dbd17e5c62f46
Avinav Saini <141184593+As262@users.noreply.github.com>
2025-02-04 16:24:58 +0530

    Update code.c

--- Print a Pyramid Pattern@@67877e1818d53eb724f435bf/code.c
+++ Print a Pyramid Pattern@@67877e1818d53eb724f435bf/code.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
-int main() {
-    int a, i, j;
-    scanf("%d", &a);
-    for (i = 1; i <= a; i++) {
-        for (j = 1; j <= a - i; j++) {
+int main(){
+    int a,i,j;
+    scanf("%i",&a);
+    for (int i=0;i<=a;i++){
+        for (int j=0;j<=a -i;j++){
             printf(" ");
         }
-        for (j = 1; j <= 2 * i - 1; j++) {
-            printf("*");
-        }
-        printf("\n");
+    for (j=1;j<=2*i-1;j++){
+                printf("*");
+    }
+    printf("\n");
     }
     return 0;
 }
